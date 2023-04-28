3a37e38a7a25df466b919bc52d7665402bd24023
Alejandro <alejandro.vergara.araneda@gmail.com>
2023-04-28 13:21:12 -0400

    termino pilas

--- listas/pila/pila.cpp
+++ listas/pila/pila.cpp
@@ -1,9 +1,40 @@
 #include "pila.hpp"
 
+int MAXSIZE=10000;
+
+
+
+tPila::tPila(){
+    maxSize=MAXSIZE;
+    top=0;
+    stackArray=new tElemPila(maxSize);
+}
+
+tPila::~tPila(){
+    delete[] stackArray;
+}
 
 int tPila::push (tElemPila item){
     if (top == maxSize) 
         return 0;
     stackArray[top++] = item;
     return 1; // inserción exitosa
+}
+
+void tPila::clear (){
+    top=0;
+}
+
+void tPila::pop(){
+    if (top==0)
+        return;
+    top--;
+}
+
+tElemPila tPila::topValue(){
+    return stackArray[top];
+}
+
+int tPila::size(){
+    return top;
 }
